dda0b5d21b9a39f3af8badfc5586ad016becff4d
Djalma Valois Filho <96025057+dvaloisBR@users.noreply.github.com>
2025-09-29 14:21:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+# Copyright (C) 2024 Djalma Valois Filho
+# 
+# This file is part of ollama_gui.
+# 
+# Ollama_Gui is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Ollama_gui is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with GitHub Deploy Assistant.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask import Flask, render_template, request, jsonify, session
 from flask_cors import CORS
 import requests
